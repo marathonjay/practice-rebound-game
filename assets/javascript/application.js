9f8d7af93e040b8bd3474fec9f0f8e330917102c
Jason Parr <parr.jason.01@gmail.com>
2021-03-17 11:12:38 -0700

    page load functions added to js.  init() and layoutPage() functions.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,3 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
+let ball;
+let paddle;
+let score;
+let playingArea;
+
+let aWidth;
+let aHeight;
+let pWidth;
+let pHeight;
+let dx = 2;
+let dy = 2;
+let pdx = 48;
+let currentScore = 0;
+let timer;
+let paddleLeft = 228;
+let ballLeft = 100;
+let ballTop = 8;
+
+
+window.addEventListener("load", init);
+
+function init() {
+  ball = document.getElementById("ball");
+  paddle = document.getElementById("paddle");
+  score = document.getElementById("score");
+  playingArea = document.getElementById("playingArea");
+  layoutPage();
+
+}
+
+function layoutPage() {
+  aWidth = innerWidth;
+  aHeight = innerHeight;
+  pWidth = aWidth - 22;
+  pHeight = aHeight - 22;
+  playingArea.style.width = pWidth + "px";
+  playingArea.style.height = pHeight + "px";
+
+  
+}
+
+
+
+
+
+
+
+
+
   // Add your Javascript Here
 })
